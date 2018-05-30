6793ebe97578d62da9576852a11570dfbd4759c9
Sam Mulube <sam@thingful.net>
2018-05-30 14:25:08 +0100

    Add simple entrypoint that attempts to wait for postgres to be ready

---
+++ docker/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+if set -o | grep -q "pipefail"; then
+  set -o pipefail
+fi
+
+RETRIES=5
+
+until nc -z postgres:5432 || [ "$RETRIES" -eq 0 ]; do
+  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts"
+  sleep 1
+done
+
+"$@"
