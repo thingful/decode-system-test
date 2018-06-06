5a4bced93e70969c693156cf36d80f98de6958e3
Sam Mulube <sam@thingful.net>
2018-06-06 13:07:48 +0100

    Fleshing out local docker-compose set up to include encoder
    Also some poorly configured monitoring

---
+++ test_script.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# This script attempts to invoke the three RPC methods exposed by the datastore
+# via Curl just for quick local sanity checking. Requires base64, curl and jq
+# tools installed and available on the current $PATH to run.
+
+echo "--> create a stream"
+curl --request "POST" \
+     --location "http://localhost:8081/twirp/encoder.Encoder/CreateStream" \
+     --header "Content-Type: application/json" \
+     --silent \
+     --data '{"broker_address":"tcp://broker:1883","device_topic":"device/sck/2047f0/readings","device_private_key":"abc123","recipient_public_key":"hij567","user_uid":"alice","location":{"longitude":0.023,"latitude":55.253},"disposition":"INDOOR"}' \
+     | jq "."
+
+sleep 5
+
+curl --request "POST" \
+     --location "http://localhost:8081/twirp/encoder.Encoder/CreateStream" \
+     --header "Content-Type: application/json" \
+     --silent \
+     --data '{"broker_address":"tcp://broker:1883","device_topic":"device/sck/f4d5fb/readings","device_private_key":"abc123","recipient_public_key":"hij567","user_uid":"alice","location":{"longitude":0.023,"latitude":55.253},"disposition":"INDOOR"}' \
+     | jq "."
