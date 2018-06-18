8e2bceebaea11ded0f416c0c1ba0f9c7953184eb
Sam Mulube <sam@thingful.net>
2018-06-18 12:41:45 +0100

    Simplify test script and add function to write data

--- test_script.sh
+++ test_script.sh
@@ -13,12 +13,12 @@ function print {
 function claim_device {
   print "claim device"
 
-  local data="{\"deviceToken\":\"$2\",\"broker\":\"tcp://broker:1883\",\"userUid\":\"$3\",\"location\":{\"longitude\":-0.2983,\"latitude\":55.5212},\"disposition\":\"INDOOR\"}"
+  local data="{\"deviceToken\":\"$1\",\"broker\":\"tcp://broker:1883\",\"userUid\":\"$2\",\"location\":{\"longitude\":-0.2983,\"latitude\":55.5212},\"disposition\":\"INDOOR\"}"
 
   echo $data | jq "."
 
   curl --request "POST" \
-       --location "$1/twirp/devicereg.DeviceRegistration/ClaimDevice" \
+       --location "http://localhost:8082/twirp/devicereg.DeviceRegistration/ClaimDevice" \
        --header "Content-Type: application/json" \
        --verbose \
        --data "$data" \
@@ -28,12 +28,12 @@ function claim_device {
 function create_stream {
   print "create a stream"
 
-  local data="{\"brokerAddress\":\"tcp://broker:1883\",\"deviceTopic\":\"device/sck/$2/readings\",\"devicePrivateKey\":\"abc123\",\"recipientPublicKey\":\"hij567\",\"userUid\":\"alice\",\"location\":{\"longitude\":0.023,\"latitude\":55.253},\"disposition\":\"INDOOR\"}"
+  local data="{\"brokerAddress\":\"tcp://broker:1883\",\"deviceTopic\":\"device/sck/$1/readings\",\"devicePrivateKey\":\"abc123\",\"recipientPublicKey\":\"hij567\",\"userUid\":\"alice\",\"location\":{\"longitude\":0.023,\"latitude\":55.253},\"disposition\":\"INDOOR\"}"
 
   echo $data | jq "."
 
   curl --request "POST" \
-       --location "$1/twirp/encoder.Encoder/CreateStream" \
+       --location "http://localhost:8081/twirp/encoder.Encoder/CreateStream" \
        --header "Content-Type: application/json" \
        --verbose \
        --data "$data" \
@@ -43,16 +43,31 @@ function create_stream {
 function delete_stream {
   print "delete a stream"
 
-  local data="{\"streamUid\":\"$2\"}"
+  local data="{\"streamUid\":\"$1\"}"
 
   echo $data | jq "."
 
   curl --request "POST" \
-       --location "$1/twirp/encoder.Encoder/DeleteStream" \
+       --location "http://localhost:8081/twirp/encoder.Encoder/DeleteStream" \
        --header "Content-Type: application/json" \
        --verbose \
        --data "$data" \
      | jq "."
+}
+
+function write_data {
+  print "write data"
+
+  local encrypted_msg=$(echo secret | base64)
+
+  local data="{\"public_key\":\"$1\",\"user_uid\":\"$2\",\"data\":\"$encrypted_msg\"}"
+
+  curl --request "POST" \
+       --location "http://localhost:8080/twirp/datastore.Datastore/WriteData" \
+       --header "Content-Type: application/json" \
+       --verbose \
+       --data "$data" \
+    | jq "."
 }
 
 "$@"
